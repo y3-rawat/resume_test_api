e40213ca4ce3c7d7a14fd76e5a9343f70c0890a6
Yash Rawat <112065069+y3-rawat@users.noreply.github.com>
2024-08-14 16:28:31 +0530

    experience error

--- calculations.py
+++ calculations.py
@@ -255,50 +255,51 @@ def Score_cards(resume_text,job_description):
 
 
     experience_error = """
-        { "output":
-        {
-                "ats_score": {
-                    "title": "Ats Score",
-                    "description": "504",
-                    "type": "integer"
-                },
-                "ats_description": {
-                    "title": "Ats Description",
-                    "description": "Error Occured!",
-                    "type": "string"
-                },
-                "ats_reason": {
-                    "title": "Ats Reason",
-                    "description": "Please Report this problem! :(",
-                    "type": "string"
-                },
-                "ats_improvementTip": {
-                    "title": "Ats Improvementtip",
-                    "description": "Error Occurred!",
-                    "type": "string"
-                },
-                "jd_score": {
-                    "title": "Jd Score",
-                    "description": "504",
-                    "type": "integer"
-                },
-                "jd_description": {
-                    "title": "Jd Description",
-                    "description": "Error Occured!",
-                    "type": "string"
-                },
-                "jd_reason": {
-                    "title": "Jd Reason",
-                    "description": "Please Report this problem! :(",
-                    "type": "string"
-                },
-                "jd_improvementTip": {
-                    "title": "Jd Improvementtip",
-                    "description": "Error Occurred!",
-                    "type": "string"
-                }
-                }
-                    }
+      {
+  "output": {
+    "ats_score": {
+      "title": "Ats Score",
+      "description": "504",
+      "type": "integer"
+    },
+    "ats_description": {
+      "title": "Ats Description",
+      "description": "Error Occurred!",
+      "type": "string"
+    },
+    "ats_reason": {
+      "title": "Ats Reason",
+      "description": "Please Report this problem! :(",
+      "type": "string"
+    },
+    "ats_improvementTip": {
+      "title": "Ats Improvement Tip",
+      "description": "Error Occurred!",
+      "type": "string"
+    },
+    "jd_score": {
+      "title": "Jd Score",
+      "description": "504",
+      "type": "integer"
+    },
+    "jd_description": {
+      "title": "Jd Description",
+      "description": "Error Occurred!",
+      "type": "string"
+    },
+    "jd_reason": {
+      "title": "Jd Reason",
+      "description": "Please Report this problem! :(",
+      "type": "string"
+    },
+    "jd_improvementTip": {
+      "title": "Jd Improvement Tip",
+      "description": "Error Occurred!",
+      "type": "string"
+    }
+  }
+}
+
                 
             """
             
