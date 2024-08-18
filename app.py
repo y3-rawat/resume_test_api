2a9ef3ba720bceeae72f6b3829343b86c56082b0
Yash Rawat <112065069+y3-rawat@users.noreply.github.com>
2024-08-18 20:26:32 +0530

    try

--- app.py
+++ app.py
@@ -89,7 +89,8 @@ def submit():
     extracted_text = request.args.get('ext-text', '')
     api = request.args.get('api','')
     print("api code",api)
-    output = get_data(job_description, additional_information, extracted_text)
+    # output = get_data(job_description, additional_information, extracted_text)
+    output = api
     return jsonify(output)
 
 if __name__ == '__main__':
